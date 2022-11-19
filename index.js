b87878f881d8f2a84a9c0665778e2233db3fcd08
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2022-11-19 23:35:53 +0300

    COmpleted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const arr =[];
+function writeCards(arr) {
+    let arr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+        arr1.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+
+    }
+return arr1;
+}
+
+function countDown(number) {
+while (number > 0) {
+     console.log(number);
+    number--;
+    }
+console.log(number);
+}
